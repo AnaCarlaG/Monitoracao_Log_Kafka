68a4ab9194e9a31c86d0703c873d1e6e74f6887c
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2022-12-03 16:27:25 -0300

    aplicacao python para vê as mensagens enviadas

---
+++ conectorapache.py
@@ -0,0 +1,18 @@
+import time
+import re
+import datetime
+from kafka import KafkaProducer as kp
+
+arquivo = open(r'/var/log/apache2/access.log','r')
+regexp = '([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"(.+?)\"'
+producer = kp(bootstrap_servers="127.0.0.1:9092")
+
+while 1:
+	linha = arquivo.readline()
+	if not linha:
+		time.sleep(5)
+	else:
+		x = re.match(regexp, linha).groups()
+		msg = bytes(str(x), encoding='ascii')
+		producer.send("apachelog", msg)
+		print("Mensagem enviada em ", datetime.datetime.now())
